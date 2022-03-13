950cdcd536a0c61096d1586c06f57a8b82001679
Hum Bruno <hsbruno1@gmail.com>
2022-03-13 21:55:32 +0000

    Finalised navbar responsiveness

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,38 @@
 const menu = document.querySelector(".header__menu");
 const navLinks = document.querySelectorAll(".link-wrapper");
 const navMenu = document.querySelectorAll(".menu-wrapper");
+const html = document.querySelector("html");
+const menuMobile = document.querySelector(".header__links-mobile");
+const navCategory = document.querySelectorAll(".category-link");
+const navItems = document.querySelectorAll(".menu-wrapper-mobile");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
+  menuMobile.classList.toggle("visible");
+
+  for (let j = 0; j < navMenu.length; j++) {
+    navItems[j].classList.remove("visible");
+  }
 });
 
 for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", () => {
+  navLinks[i].addEventListener("mouseover", () => {
     for (let j = 0; j < navMenu.length; j++) {
       navMenu[j].classList.remove("visible");
     }
 
     navMenu[i].classList.add("visible");
+  });
+}
+
+html.addEventListener("click", () => {
+  for (let i = 0; i < navMenu.length; i++) {
+    navMenu[i].classList.remove("visible");
+  }
+});
+
+for (let i = 0; i < navCategory.length; i++) {
+  navCategory[i].addEventListener("click", () => {
+    navItems[i].classList.toggle("visible");
   });
 }
